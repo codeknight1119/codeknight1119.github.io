046f40b5d9c583c733652ec831b13160ab5ab7c3
codeknight1119 <code.knight1119@gmail.com>
2026-03-31 15:56:07 -0400

    Push for now

---
+++ chat/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.x.x/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.x.x/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuOdufK2UCl9m6iZa35SUQSRF-9HZHcD8",
+  authDomain: "chatroom4friends-522bd.firebaseapp.com",
+  projectId: "chatroom4friends-522bd",
+  storageBucket: "chatroom4friends-522bd.firebasestorage.app",
+  messagingSenderId: "26805339142",
+  appId: "1:26805339142:web:86ff21490be804a16eaf87",
+  measurementId: "G-EXFX8QFGGF"
+};
+
+firebase.initializeApp({firebaseConfig});
+const messaging = firebase.messaging();
+
+// This handles background notifications
+messaging.onBackgroundMessage((payload) => {
+  console.log('Background message received: ', payload);
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, { body, icon: '/icon-192.png' });
+});

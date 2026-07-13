32cb6d12ed3dbe5b6bad89afae42b31e3cf626e9
codeknight1119 <code.knight1119@gmail.com>
2026-07-13 19:22:02 -0400

    update

--- The Tavern/main/app.js
+++ The Tavern/main/app.js
@@ -190,8 +190,11 @@ const mainContentArea = document.getElementById("mainContentArea")
 
 function newBoard(title, body){
     const newBoard = document.getElementById("board:template").content.cloneNode(true)
-    newBoard.querySelector(".board-title").innerText = title || "Title"
-    newBoard.querySelector(".board-body").innerText = body || "Type announcement"
+    const titleText = newBoard.querySelector(".board-title")
+    const bodyText = newBoard.querySelector(".board-body")
+    titleText.contenteditable = bodyText.contenteditable = permissions.includes("officer")
+    titleText.innerText = title || "Title"
+    newBoard.innerText = body || "Type announcement"
     mainContentArea.prepend(newBoard)
 
 }

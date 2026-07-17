39c6c52820139d1925898487670ac39dc86ba69a
codeknight1119 <code.knight1119@gmail.com>
2026-07-17 10:18:51 -0400

    update

--- The Tavern/firebaseUtils.js
+++ The Tavern/firebaseUtils.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const auth = getAuth(app);
-const analytics = getAnalytics(app)
+const analytics = getAnalytics(app, {config: { 'debug_mode': true }})
 
 export const loginGoogle = async () => {
     const provider = new GoogleAuthProvider();

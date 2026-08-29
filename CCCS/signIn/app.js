4ae562b5c04575fd6ac919d369fe6d9ebfd72cd2
codeknight1119 <code.knight1119@gmail.com>
2026-08-28 21:08:17 -0400

    update

--- CCCS/signIn/app.js
+++ CCCS/signIn/app.js
@@ -1,23 +1,48 @@
 Quagga.init({
   inputStream: {
     type: "LiveStream",
-    target: document.querySelector('#scanner-container')
+    target: document.querySelector("#scanner-container"),
+
+    constraints: {
+      width: { min: 1280 },
+      height: { min: 720 },
+      facingMode: "environment"
+    },
+
+    area: {
+      top: "20%",
+      right: "10%",
+      left: "10%",
+      bottom: "20%"
+    }
   },
+
+  locator: {
+    patchSize: "medium",
+    halfSample: false
+  },
+
   decoder: {
-    readers: ["code_128_reader", "code_39_reader"]
-  }
-}, function(err) {
+    readers: [
+      "code_128_reader",
+      "code_39_reader"
+    ]
+  },
+
+  locate: true
+}, function (err) {
   if (err) {
     console.error(err);
     return;
   }
+
   console.log("Initialization finished. Ready to start");
   Quagga.start();
 });
 
-Quagga.onDetected(function(result) {
-    console.log(result)
+Quagga.onDetected(function (result) {
+  console.log(result);
+
   const code = result.codeResult.code;
   console.log("Barcode detected:", code);
-  //alert("Found barcode: " + code);
 });
